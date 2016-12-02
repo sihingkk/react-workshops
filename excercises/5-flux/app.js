fd9745db50df2a7a7645994c56343ff0e24bd989
Krzysztof Kaczmarek <krzysztof.kaczmarek@me.com>
2016-12-02 09:35:51 +0100

    url

--- excercises/5-flux/app.js
+++ excercises/5-flux/app.js
@@ -1,7 +1,7 @@
 // Cwiczenie:
 //
 // Dodaj funkcjonalnosc usuwania kontaktu - zrob to zgodnie z fluxem
-// hint: uzyj `xhr.deleteJSON(url)` gdzie url to
+// hint: uzyj `xhr.deleteJSON(url)` gdzie url to `/contacts/:id` 
 
 var React = require('react');
 var App = require('./app/components/App');
